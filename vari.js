ff09953d8a94726c2c41cc37550fbc316ee5b374
mahmudul-hasan <mhmahmudul1211@gmail.com>
2024-09-18 22:31:52 +0600

    first commit

---
+++ vari.js
@@ -0,0 +1,28 @@
+// var is old --> we can change var value 
+var car = "bike";
+car = "bus";
+console.log(car)
+
+
+// const not change able 
+const name = "Mahmudul" //in string we must use double quote ""
+// name = "molla"; will show error because const variable value is not changeable.
+console.log(name)
+console.log(typeof name) // check the type of variable
+
+// let --> only use where declare there specially in loop [changeable value]
+let age = 99;
+age = 23; // the value of age variable will be 23 from 99
+console.log(age)
+console.log(typeof age) // check the type of variable
+
+
+// Boolean  type variable
+var isPoor = false;
+console.log(isPoor) 
+
+var isPassed = true;
+console.log(isPassed)
+console.log(typeof isPassed) // check the type of variable
+
+
